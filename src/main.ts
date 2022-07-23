588a92b5438c7042f59fd68afca70801f541c69d
WuQiang <wuqiang_19950420@outlook.com>
2022-07-23 22:32:05 +0800

    删除touchmove

--- src/main.ts
+++ src/main.ts
@@ -11,10 +11,10 @@ vantRegister(app);
 app.use(store);
 app.use(router);
 app.mount('#app');
-document.body.addEventListener(
-  'touchmove',
-  function (e) {
-    e.preventDefault(); //阻止默认的处理方式(阻止下拉滑动的效果)
-  },
-  { passive: false }
-); //passive 参数不能省略，用来兼容ios和android
+// document.body.addEventListener(
+//   'touchmove',
+//   function (e) {
+//     e.preventDefault();
+//   },
+//   { passive: false }
+// );
